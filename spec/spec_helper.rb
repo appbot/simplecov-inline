30f743b8cc429067b08ad7deab7727aa38b67d65
tris <gondalez@gmail.com>
2024-08-20 16:23:12 +0800

    Enable simplecov and enforce 100% branching coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  primary_coverage :branch
+  minimum_coverage line: 100, branch: 100
+end
+
 require 'simplecov/inline'
 
 RSpec.configure do |config|
