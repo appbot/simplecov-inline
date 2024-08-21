157948013c0805dc7ab0fb8d346255f0e229913a
tris <gondalez@gmail.com>
2024-08-21 13:25:37 +0800

    Add class to configure rspec and rails

--- lib/simplecov/inline.rb
+++ lib/simplecov/inline.rb
@@ -1,6 +1,7 @@
 require_relative 'inline/version'
 require_relative 'inline/formatter'
 require_relative 'inline/rspec_formatter_skip_on_failure'
+require_relative 'inline/integration'
 
 module SimpleCov
   module Inline
